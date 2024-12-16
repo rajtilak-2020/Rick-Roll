ccf7367c122a36c4c40d9726ab4a81817b71ad42
K Raj Tilak <rajtilak1062020@gmail.com>
2024-12-16 10:59:16 +0530

    Add files via upload

---
+++ Rickroll2.bat
@@ -0,0 +1,44 @@
+@echo off
+title Never Gonna Give You Up
+mode con: cols=80 lines=25
+color 0A
+echo.
+echo   NNNNNNNN        NNNNNNNN            GGGGGGGGGGGGG          
+echo   N:::::::N       N::::::N         GGG::::::::::::G          
+echo   N::::::::N      N::::::N       GG:::::::::::::::G          
+echo   N:::::::::N     N::::::N      G:::::GGGGGGGG::::G          
+echo   N::::::::::N    N::::::N     G:::::G       GGGGGG   ooooooo
+echo   N:::::::::::N   N::::::N     G:::::G               oo     oo
+echo   N:::::::N::::N  N::::::N     G:::::G               oo     oo
+echo   N::::::N N::::N N::::::N     G:::::G    GGGGGGGGG  oo     oo
+echo   N::::::N  N::::N:::::::N     G:::::G    G::::::::G oo     oo
+echo   N::::::N   N:::::::::::N     G:::::G    GGGGG::::G oo     oo
+echo   N::::::N    N::::::::::N     G:::::G        G::::G oo     oo
+echo   N::::::N     N:::::::::N      G:::::G       G::::G oo     oo
+echo   N::::::N      N::::::::N       G:::::GGGGGGGG::::G  ooooooo 
+echo   N::::::N       N:::::::N        GG::::::::::::::G          
+echo   N::::::N        N::::::N          GGG::::::GGG:::G          
+echo   NNNNNNNN         NNNNNNN             GGGGGG   GGGG          
+echo.
+echo   N E V E R   G O N N A   G I V E   Y O U   U P  ! 
+echo.
+echo   Press any key to start singing along!
+pause >nul
+cls
+
+:: Simulate song lyrics
+echo ♪ Never gonna give you up ♪
+timeout /t 2 >nul
+echo ♪ Never gonna let you down ♪
+timeout /t 2 >nul
+echo ♪ Never gonna run around and desert you ♪
+timeout /t 2 >nul
+echo ♪ Never gonna make you cry ♪
+timeout /t 2 >nul
+echo ♪ Never gonna say goodbye ♪
+timeout /t 2 >nul
+echo ♪ Never gonna tell a lie and hurt you ♪
+timeout /t 2 >nul
+echo.
+echo   You just got Rickrolled! :)
+pause
